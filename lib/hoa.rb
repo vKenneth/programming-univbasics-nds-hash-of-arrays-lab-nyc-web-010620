4ab4d3f16d808b8577efe8416f7bfac6f815b880
K e n V <kenneth_valdivia@yahoo.com>
2019-12-28 23:46:37 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -22,3 +22,9 @@ third_earthers = {
   name_3: ["Mumm-Ra"],
 }
 
+daily_weather = {
+  temperature: [75, 80, 72],
+  precipitation: [0.0, 0.01, 0.03],
+  wind_velocity: [4, 3, 2],
+  barometric_pressure: [30.32, 30.30, 30.20]
+}
