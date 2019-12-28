6075a163e0e4fda3284cd8ab955198698e2e8cbe
K e n V <kenneth_valdivia@yahoo.com>
2019-12-28 23:49:20 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -22,9 +22,9 @@ third_earthers = {
   name_3: ["Mumm-Ra"],
 }
 
-daily_weather = {
-  temperature: [75, 80, 72],
-  precipitation: [0.0, 0.01, 0.03],
-  wind_velocity: [4, 3, 2],
-  barometric_pressure: [30.32, 30.30, 30.20]
+the_jetsons = {
+  jetson_1: ["George"],
+  jetson_2: ["Jane"],
+  jetson_3: ["Judy"],
+  jetson_4: ["Elroy"]
 }
