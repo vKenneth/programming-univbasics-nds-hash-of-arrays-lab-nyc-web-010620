f30c5b27bbdd41af03a83a39e00af57a3aec6a5b
K e n V <kenneth_valdivia@yahoo.com>
2019-12-28 23:43:50 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,4 +9,9 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
 end
-
+# movies and tv-shows
+Alvin_and_the_chipmunks = {
+  chipmunk_1: ["Alvin"],
+  chipmunck_2: ["Simon"],
+  chipmunck_3: ["Theodore"],
+}
